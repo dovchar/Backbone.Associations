dd09c0f36beda90d26d1fe6ae72bc78379b80811
Dmytro Nemoga <Dmytro.Nemoga@gmail.com>
2012-09-28 00:14:37 +0300

    Code refactoring

--- src/backbone.associations.js
+++ src/backbone.associations.js
@@ -4,71 +4,81 @@
     Backbone.Associations = {};
 
     /**
-     * Backbone.Associations.BelongsTo
+     * @class Backbone.Associations.BelongsTo
      */
-    var BelongsTo = Backbone.Associations.BelongsTo = function (storage) {
+    var BelongsTo = Backbone.Associations.BelongsTo = {};
 
-    };
+    _.extend(Backbone.Associations.BelongsTo = function (storage) {
 
-    _.extend(BelongsTo.prototype, {
+    }, BelongsTo, {
+        prototype: _.extend(BelongsTo.prototype, {
 
-    });
-
-    /**
-     * Backbone.Associations.HasMany
-     */
-    var HasMany = Backbone.Associations.HasMany = function (storage) {
-
-    };
-
-    _.extend(HasMany.prototype, {
-
-    });
-
-    /**
-     * Backbone.Associations.HasOne
-     */
-    var HasOne = Backbone.Associations.HasOne = function (storage) {
-
-    };
-
-    _.extend(HasOne.prototype, {
-
-    });
-
-    /**
-     * Backbone.Model
-     */
-    var Model = Backbone.Model;
-
-    _.extend(Model.prototype, {
-        belongsTo: {},
-
-        hasMany: {},
-
-        hasOne: {},
-
-        toJSON: _.wrap(Model.prototype.toJSON, function (toJSON, options) {
-            // Ensure options
-            options = _.extend({
-                associations: false
-            }, options);
-
-            var attributes = toJSON.call(this, options);
-
-//            if (options.associations) {
-//
-//            }
-
-            return attributes;
         })
     });
 
     /**
-     * Backbone.sync
+     * @class Backbone.Associations.HasMany
+     */
+    var HasMany = Backbone.Associations.HasMany = {};
+
+    _.extend(Backbone.Associations.HasMany = function (storage) {
+
+    }, HasMany, {
+        prototype: _.extend(HasMany.prototype, {
+
+        })
+    });
+
+    /**
+     * @class Backbone.Associations.HasOne
+     */
+    var HasOne = Backbone.Associations.HasOne = {};
+
+    _.extend(Backbone.Associations.HasOne = function (storage) {
+
+    }, HasOne, {
+        prototype: _.extend(HasOne.prototype, {
+
+        })
+    });
+
+    /**
+     * @class Backbone.Model
+     */
+    var Model = Backbone.Model;
+
+    _.extend(Backbone.Model = function () {
+        Model.apply(this, arguments);
+    }, Model, {
+        prototype: _.extend(Model.prototype, {
+            belongsTo: {},
+
+            hasMany: {},
+
+            hasOne: {},
+
+            toJSON: _.wrap(Model.prototype.toJSON, function (toJSON, options) {
+                // Ensure options
+                options = _.extend({
+                    associations: false
+                }, options);
+
+                var attributes = toJSON.call(this, options);
+
+//                if (options.associations) {
+//
+//                }
+
+                return attributes;
+            })
+        })
+    });
+
+    /**
+     * @function Backbone.sync
      */
     Backbone.sync = _.wrap(Backbone.sync, function (sync, method, context, options) {
-        // Check for current method
+        // Run native "sync" for all methods except "read"
         if (method !== 'read') {
             return sync.call(this, method, context, options);
         }
@@ -82,27 +92,27 @@
                 // Clear XHR cache
                 delete this._xhrCache;
 
-                // Perform custom callback function
+                // Run custom callback
                 if (_.isFunction(complete)) {
                     complete.call(this, this, jqXHR, textStatus);
                 }
             }, context),
 
             success = _.bind(function (success, data, textStatus, jqXHR) {
-                // Resolve deferred call
+                // Resolve deferred
                 deferred.resolve(data, textStatus, jqXHR);
 
-                // Perform custom callback function
+                // Run custom callback
                 if (_.isFunction(success)) {
                     success.call(this, this, data, options);
                 }
             }, context),
 
             error = _.bind(function (error, jqXHR, textStatus, errorThrown) {
-                // Reject deferred call
+                // Reject deferred
                 deferred.reject(jqXHR, textStatus, errorThrown);
 
-                // Perform custom callback function
+                // Run custom callback
                 if (_.isFunction(error)) {
                     error.call(this, this, jqXHR, options);
                 }
@@ -111,13 +121,14 @@
             done = _.bind(function () {
                 // Prevent request duplication
                 if (_.isUndefined(this._xhrCache)) {
+                    // Run native "sync"
                     this._xhrCache = sync.call(this, 'read', context, options);
                 } else {
                     this._xhrCache.then(deferred.resolve, deferred.reject);
                 }
             }, context);
 
-        // Wrap custom callbacks
+        // Wrap native callbacks
         _.extend(options, {
             complete: _.wrap(options.complete, complete),
             success: _.wrap(options.success, success),
